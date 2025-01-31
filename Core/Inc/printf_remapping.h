c92f4f0eefd6aa9bdf31b59fbbce57230e0c53e9
HYEON <sunki1123@naver.com>
2025-01-31 15:53:02 +0900

    STM BLE Code Study Project Ready

---
+++ Core/Inc/printf_remapping.h
@@ -0,0 +1,20 @@
+
+#ifndef ___PRINTF___REMAPPING__H___
+#define ___PRINTF___REMAPPING__H___
+
+#define cli_buffer_maxlen 1024
+extern uint8_t cli_line_data[cli_buffer_maxlen];
+extern uint8_t cli_line_datalen;
+extern uint8_t cli_enter_avilable;
+extern uint8_t cli_line_available;
+
+
+#define PRINTF_REMAPPING_USE_USB_VCOM 1
+#define PRINTF_REMAPPING_USE_USART 0
+
+extern int __custom_printf__(const char *format, ...);
+
+void printf_remapping_init();
+void printf_remapping_process();
+
+#endif
